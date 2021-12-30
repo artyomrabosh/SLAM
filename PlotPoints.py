0a534e2ac10f0787da490eb69517146c40daa784
emerso2000 <83967836+emerso2000@users.noreply.github.com>
2021-12-29 22:32:14 -0500

    Update PlotPoints.py

--- PlotPoints.py
+++ PlotPoints.py
@@ -1,54 +1,60 @@
 import OpenGL.GL as gl
 import pangolin
-
 import numpy as np
+from Extract import data 
+import threading
+from Extract import Slam
+import time
+import cv2
 
-class display3D():
-
-    def __init__(self):
-        #self.Coordinates = newCoordinates
-        #pass in newCoordinates from other class
-        pass
+class display3D:
+    def __init__(self, newCoordinates):
+        self.Coordinates = newCoordinates
+        self.viewer()
 
     def viewer(self):
-        pangolin.CreateWindowAndBind('Main', 640, 480)
+        pangolin.CreateWindowAndBind('Plot', 640, 480)
         gl.glEnable(gl.GL_DEPTH_TEST)
 
         # Define Projection and initial ModelView matrix
         self.scam = pangolin.OpenGlRenderState(
-            pangolin.ProjectionMatrix(640, 480, 420, 420, 640//2, 480//2, 0.2, 10000),
-            pangolin.ModelViewLookAt(0, -10, -8,
-                                0, 0, 0,
-                                0, -1, 0))
+        pangolin.ProjectionMatrix(640, 480, 420, 420, 320, 240, 0.2, 200),
+        pangolin.ModelViewLookAt(-2, 2, -2, 0, 0, 0, pangolin.AxisDirection.AxisY))
+
         handler = pangolin.Handler3D(self.scam)
 
         # Create Interactive View in window
         self.dcam = pangolin.CreateDisplay()
-        self.dcam.SetBounds(0.0, 1.0, 0.0, 1.0, 640.0/480.0)
+        self.dcam.SetBounds(0.0, 1.0, 0.0, 1.0, -640.0/480.0)
         self.dcam.SetHandler(handler)
+
         self.dcam.Activate()
         
     def refresh(self):
-        while not pangolin.ShouldQuit():
+        if True:
             gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
             gl.glClearColor(0.0, 0.0, 0.0, 1.0)            
             self.dcam.Activate(self.scam)
-                        
-            # Draw camera
-            gl.glColor3f(0.0, 1.0, 0.0)
-            #pangolin.DrawCameras(pose)
-            #add in pose value in main
-
 
             gl.glPointSize(5)
             gl.glColor3f(1.0, 0.0, 0.0)
-            #pangolin.DrawPoints(newCoordinates)
-            
-        pangolin.FinishFrame()
+            # # access numpy array directly(without copying data), array should be contiguous.
+            data.data = data.data.squeeze()
+            pangolin.DrawPoints(data.data)
+            pangolin.FinishFrame()
 
-    def main(self):
-        self.viewer()
-        self.refresh()
 
-od = display3D()
-od.main()
+try:
+    slam = Slam()
+    od = display3D(data.data)
+
+    slam.updateCoordinates()
+    od.dcam.Activate(od.scam)
+    while not pangolin.ShouldQuit() and not (cv2.waitKey(1) & 0xFF == ord('q')):
+        if slam.vid1.isOpened() and slam.vid2.isOpened():
+            slam.camera()
+        od.refresh()
+
+except Exception as e:
+    print(e)
+    raise
